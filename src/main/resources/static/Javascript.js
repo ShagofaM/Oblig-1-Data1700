bb97ea23bca1a6d12cfe0bbcca968a08d27cba3f
ShagofaM <144257642+ShagofaM@users.noreply.github.com>
2024-03-06 14:28:56 +0100

    Endret javascript slik at errormeldingene blir fjernet etter riktig input blir lagt inn

--- src/main/resources/static/Javascript.js
+++ src/main/resources/static/Javascript.js
@@ -1,52 +1,44 @@
 let billetter = [], ut = "<table><tr>" +
-
     "<th>Filmer</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>" + "</tr>";
 
-function kjopKnapp(){
-    const billett={
-
+function kjopKnapp() {
+    const billett = {
         Filmer: document.getElementById("Filmer").value,
         Antall: document.getElementById("Antall").value,
         Fornavn: document.getElementById("Fornavn").value,
         Etternavn: document.getElementById("Etternavn").value,
         Telefonnr: document.getElementById("Telefonnr").value,
         Epost: document.getElementById("Epost").value
-    };
+    }
 
-    if (!billett.Antall || billett.Fornavn || !billett.Etternavn || !billett.Telefonnr || !billett.Epost) {
-        document.getElementById("AntallError").innerHTML="Vennligst fyll inn antall billetter";
-        document.getElementById("FornavnError").innerHTML="Vennligst fyll inn fornavn";
-        document.getElementById("EtternavnError").innerHTML="Vennligst fyll inn etternavn";
-        document.getElementById("TelefonnrError").innerHTML="Vennligst fyll inn telefonnummer";
-        document.getElementById("EpostError").innerHTML="Vennligst fyll inn epost adresse";
-
+    if (!billett.Antall || !billett.Fornavn || !billett.Etternavn || !billett.Telefonnr || !billett.Epost) {
+        document.getElementById("AntallError").innerHTML = "Vennligst fyll inn antall billetter";
+        document.getElementById("FornavnError").innerHTML = "Vennligst fyll inn fornavn";
+        document.getElementById("EtternavnError").innerHTML = "Vennligst fyll inn etternavn";
+        document.getElementById("TelefonnrError").innerHTML = "Vennligst fyll inn telefonnummer";
+        document.getElementById("EpostError").innerHTML = "Vennligst fyll inn epost adresse";
     } else {
         billetter.push(billett);
 
-        for(let liste of billetter){
-            ut+="<tr>"
-            ut+="<td>" + liste.Filmer + "</td><td>"+ liste.Antall +"</td><td>"+ liste.Fornavn+"</td>"+
-                "<td>" + liste.Etternavn+ "</td><td>" + liste.Telefonnr+"</td><td>"+liste.Epost+"</td>";
-            ut+="</tr>"
-
+        for (let liste of billetter) {
+            ut += "<tr>"
+            ut += "<td>" + liste.Filmer + "</td><td>" + liste.Antall + "</td><td>" + liste.Fornavn + "</td>" +
+                "<td>" + liste.Etternavn + "</td><td>" + liste.Telefonnr + "</td><td>" + liste.Epost + "</td>";
+            ut += "</tr>"
         }
-        document.getElementById("ut").innerHTML=ut;
+        document.getElementById("ut").innerHTML = ut;
 
-        billetter=[];
+        billetter = [];
 
-        document.getElementById("AntallError").innerHTML="";
-        document.getElementById("FornavnError").innerHTML="";
-        document.getElementById("EtternavnError").innerHTML="";
-        document.getElementById("TelefonnrError").innerHTML="";
-        document.getElementById("EpostError").innerHTML="";
+        document.getElementById("AntallError").innerHTML = "";
+        document.getElementById("FornavnError").innerHTML = "";
+        document.getElementById("EtternavnError").innerHTML = "";
+        document.getElementById("TelefonnrError").innerHTML = "";
+        document.getElementById("EpostError").innerHTML = "";
     }
-        document.getElementById("AntallError").valueL="";
-        document.getElementById("FornavnError").valueL="";
-        document.getElementById("EtternavnError").valueL="";
-        document.getElementById("TelefonnrError").valueL="";
-        document.getElementById("EpostError").valueL="";
-    }
-    function slettKnapp() {
-    document.getElementById("ut").innerHTML="";
-    billetter=[];
-    }
+}
+
+function slettKnapp() {
+    document.getElementById("ut").innerHTML = "";
+    billetter = [];
+}
